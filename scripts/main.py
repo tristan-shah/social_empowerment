01116880b21a0497227c642f57a3825dd56ceb27
Tristan <123.tristan.shah@gmail.com>
2025-05-25 22:07:25 -0500

    tihgn

--- scripts/main.py
+++ scripts/main.py
@@ -8,8 +8,6 @@ from jax import numpy as jnp
 from jax import Array
 import imageio
 
-import matplotlib.pyplot as plt
-
 # Enable higher precision (critical for second derivatives)
 jax.config.update('jax_enable_x64', True)
 jax.config.update('jax_traceback_filtering', 'off')
@@ -33,26 +31,28 @@ class Dynamics:
 
     def step(self, xt: Array, ut: Array):
 
-        qpos = xt[:self.mjx_model.nq]
-        qvel = xt[self.mjx_model.nq:]
-        data = mjx.make_data(self.model).replace(qpos = qpos, qvel = qvel, ctrl = ut)
-        data = self.mjx_step(self.mjx_model, data)
-        return jnp.concatenate([data.qpos, data.qvel])
+        qpos, qvel = split_state(xt, self.nq)
+
+        mjx_data = mjx.make_data(self.mjx_model).replace(qpos = qpos, qvel = qvel, ctrl = ut)
+
+        mjx_data = self.mjx_step(self.mjx_model, mjx_data)
+        return jnp.concatenate([mjx_data.qpos, mjx_data.qvel])
     
     def render(self, X: Array, path: str):
 
         renderer = mujoco.Renderer(self.model, height = 720, width = 1280)
+        # renderer = mujoco.Renderer(self.model)
 
         # Create a free camera
         camera = mujoco.MjvCamera()
         camera.lookat = jnp.array([0.0, 0.0, 1.2])  # Point the camera is looking at (x, y, z)
         camera.distance = 3.0  # Distance from the lookat point
         camera.azimuth = 90.0  # Horizontal angle (degrees, 0 = looking along +x)
-        camera.elevation = 10.0  # Vertical angle (degrees, -90 = straight down)
+        # camera.elevation = 10.0  # Vertical angle (degrees, -90 = straight down)
+        camera.elevation = -10.0  # Vertical angle (degrees, -90 = straight down)
 
         data = mujoco.MjData(self.model)
         writer = imageio.get_writer(path, fps = 60)
-        # frames = []
 
         for t in range(X.shape[0]):
 
@@ -61,7 +61,6 @@ class Dynamics:
             
             renderer.update_scene(data, camera = camera)
             writer.append_data(renderer.render())
-            # frames.append(renderer.render())
 
         renderer.close()
         writer.close()
@@ -72,16 +71,24 @@ class Dynamics:
 if __name__ == '__main__':
 
     dyn = Dynamics(path = 'xml/pendulum.xml')
+    # dyn = Dynamics(path = 'mujoco_menagerie/franka_emika_panda/mjx_scene.xml')
     xt = jnp.zeros(dyn.state_dim)
     ut = jnp.zeros(dyn.control_dim)
     xt = xt.at[0].set(3.1)
 
-    T = 500
-    X = jnp.zeros((T, dyn.state_dim))
+    # ut = ut.at[1].set(0.5)
+    # ut = ut.at[4].set(0.5)
+
+
+    T = 300
+    X = jnp.zeros((T+1, dyn.state_dim))
+
+    X = X.at[0].set(xt)
 
     for t in range(T):
         xt = dyn.step(xt, ut)
         print(xt)
-        X = X.at[t].set(xt)
+        X = X.at[t+1].set(xt)
 
     dyn.render(X, path = 'pendulum.mp4')
+    # dyn.render(X, path = 'panda.mp4')
