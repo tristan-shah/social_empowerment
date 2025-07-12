d0fc2e78a28c6ef04d9700683bd32115a58edc25
Tristan Shah <tristanshah@Tristans-MacBook-Pro.local>
2025-07-12 09:11:01 -0700

    small signs of go2 intellignce

--- scripts/ilqr.py
+++ scripts/ilqr.py
@@ -13,17 +13,29 @@ from soc_emp.empowerment import unroll
 from soc_emp.utils import split_state, smooth_angle_wrap
 
 def sub_quat(q1: Array, q2: Array):
+    '''
+    Takes in two scalar-last quaternions and performs: q1 - q2. Returns a scalar first quaternion.
+    '''
+
+    ## convert to scalar last
     q1 = jnp.roll(q1, shift=-1, axis=-1)
     q2 = jnp.roll(q2, shift=-1, axis=-1)
 
+    ## construct Rotation object
     q1 = Rotation.from_quat(q1)
     q2 = Rotation.from_quat(q2)
+
+    ## perform subtraction
     q = (q1 * q2.inv()).as_quat(scalar_first = True)
-    return jnp.where(q[0] < 0, -q, q)
+    # return jnp.where(q[0] < 0, -q, q)
+    return q
 
 def compute_delta_x(m: mjx.Model, xt: Array, xt_bar: Array):
 
+    ## initialize a vector to hold the difference: xt - xt_bar
     delta_x = jnp.zeros_like(xt)
+
+    ## separate position from velocity. position requires special handling
     qpos_xt, qvel_xt = split_state(xt, m.nq)
     qpos_xt_bar, qvel_xt_bar = split_state(xt_bar, m.nq)
 
@@ -82,7 +94,8 @@ class iLQR:
         U_new = jnp.zeros_like(k)
 
         for t in range(T):
-            delta_x = compute_delta_x(self.dyn.mjx_model, X_new[t], X[t])
+            delta_x = X_new[t] - X[t]
+            # delta_x = compute_delta_x(self.dyn.mjx_model, X_new[t], X[t])
 
             ut = k[t] + K[t] @ delta_x
             ut = ut.clip(low, high)
@@ -113,7 +126,8 @@ class iLQR:
         for t in reversed(range(e.shape[0])):
             P = L + M
             ## inverse term
-            S = jnp.linalg.inv(R + B[t].T @ P @ B[t])
+            S = jnp.linalg.inv(R + B[t].T @ P @ B[t] + jnp.eye(du) * 1e-6)
+            # S = jnp.linalg.inv(R + B[t].T @ P @ B[t])
             ## gradient of policy
             grad_pi = - S @ B[t].T @ P @ A[t]
             ## compute gains
@@ -133,10 +147,9 @@ if __name__ == '__main__':
 
     integrator = 'implicitfast'
     # integrator = 'euler'
-    # integrator = 'rk4'
-    # dyn = Dynamics(path = 'xml/unitree_go2/scene_mjx.xml', integrator = integrator, dt = 0.001)
+    dyn = Dynamics(path = 'xml/unitree_go2/scene_mjx.xml', integrator = integrator, dt = 0.001)
     # dyn = Dynamics(path = 'xml/unitree_g1/scene_mjx.xml', integrator = integrator, dt = 0.001)
-    dyn = Dynamics(path = 'xml/custom/ball.xml', integrator = integrator)
+    # dyn = Dynamics(path = 'xml/custom/ball.xml', integrator = integrator)
 
     print(f'State dimention: {dyn.state_dim}')
     print(f'Control dimention: {dyn.control_dim}')
@@ -148,30 +161,41 @@ if __name__ == '__main__':
     batch_linearize = jax.vmap(dyn.linearize)
 
     xt = dyn.init_state()
-    xt = xt.at[2].set(0.1)
-    # home = dyn.model.keyframe('home')
-    # xt = xt.at[:dyn.nq].set(home.qpos)
+    home = dyn.model.keyframe('home')
+    xt = xt.at[:dyn.nq].set(home.qpos)
 
     for jnt_type, jnt_qposadr in zip(dyn.mjx_model.jnt_type, dyn.mjx_model.jnt_qposadr):
         print(mjx.JointType(jnt_type), jnt_qposadr)
 
-    steps = 1000
-    # goal_qpos = jnp.array(home.qpos)
-    goal_qpos = jnp.zeros(dyn.nq)
-    goal_qpos = goal_qpos.at[0].set(1.0) ## x
+    steps = 300 #1000
+    goal_qpos = jnp.array(home.qpos)
+    # goal_qpos = goal_qpos.at[0].set(-2.0) ## x
+    # goal_qpos = goal_qpos.at[1].set(-1.0) ## y
+    
+    goal_qpos = goal_qpos.at[0].set(0.1) ## x
     goal_qpos = goal_qpos.at[1].set(0.0) ## y
-    goal_qpos = goal_qpos.at[2].set(0.1) ## z
-    goal = jnp.concatenate([goal_qpos, jnp.zeros(dyn.nv)])
+
+    goal_qvel = jnp.zeros(dyn.nv)
+    goal_qvel = goal_qvel.at[0].set(1.0)
+    goal = jnp.concatenate([goal_qpos, goal_qvel])
 
     Q = jnp.zeros((dyn.state_dim, dyn.state_dim))
     Q = Q.at[0, 0].set(1.0)
     Q = Q.at[1, 1].set(1.0)
-    # R = jnp.eye(dyn.control_dim) * 0.0001
-    R = jnp.eye(dyn.control_dim) * 0.00001
+
+    '''for ball'''
+    # Q = Q.at[dyn.nq:, dyn.nq:].set(jnp.eye(dyn.nv) * 0.001) ## velocity penalty
+    # R = jnp.eye(dyn.control_dim) * 0.00001
+
+    '''for go'''
+    # Q = Q.at[2, 2].set(1.0)
+    Q = Q.at[dyn.nq:, dyn.nq:].set(jnp.eye(dyn.nv) * 0.00001) ## velocity penalty
+    R = jnp.eye(dyn.control_dim) * 0.001
     
     ilqr = iLQR(dyn, Q, R)
 
-    U = jnp.zeros((steps, dyn.control_dim))
+    U = jnp.tile(home.ctrl[None, :], (steps, 1))
+    # U = jnp.zeros((steps, dyn.control_dim))
     X = unroll(dyn, xt, U)
 
     cost = []
@@ -194,7 +218,8 @@ if __name__ == '__main__':
 
     for i in range(U.shape[1]):
         ax[1].plot(U[:, i])
-    fig.savefig('ball_cost.png', dpi = 300)
+    # fig.savefig('ball_cost.png', dpi = 300)
+    fig.savefig('go_cost.png', dpi = 300)
 
-    # dyn.render(X, path = 'ball.mp4', skip = 10)
-    dyn.render(X, path = 'ball.mp4', skip = 10, elevation = -90)
+    # dyn.render(X, path = 'ball.mp4', skip = 10, elevation = -90)
+    dyn.render(X, path = 'go.mp4', skip = 10)
