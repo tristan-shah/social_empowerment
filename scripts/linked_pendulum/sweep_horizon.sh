1130e7e8e5b502a2b507ac69cd04270dc7b40576
Tristan Shah <tristanshah@Tristans-MacBook-Pro.local>
2025-09-15 14:33:56 -0500

    uptaded batch script

--- scripts/linked_pendulum/sweep_horizon.sh
+++ scripts/linked_pendulum/sweep_horizon.sh
@@ -14,7 +14,7 @@ conda activate soc_emp
 export PYTHONUNBUFFERED=1  # Add this
 
 # Run your Python script
-python scripts/linked_pendulum/sweep_power.py --power 1.0 --alpha 0.01 --observation_noise 1.0
-# python scripts/linked_pendulum/sweep_power.py --horizon 100 --alpha 0.01 --observation_noise 0.1
-# python scripts/linked_pendulum/sweep_power.py --horizon 100 --alpha 0.01 --observation_noise 0.01
-# python scripts/linked_pendulum/sweep_power.py --horizon 100 --alpha 0.01 --observation_noise 0.001
+python scripts/linked_pendulum/sweep_horizon.py --power 1.0 --alpha 0.01 --observation_noise 1.0
+# python scripts/linked_pendulum/sweep_horizon.py --horizon 100 --alpha 0.01 --observation_noise 0.1
+# python scripts/linked_pendulum/sweep_horizon.py --horizon 100 --alpha 0.01 --observation_noise 0.01
+# python scripts/linked_pendulum/sweep_horizon.py --horizon 100 --alpha 0.01 --observation_noise 0.001
