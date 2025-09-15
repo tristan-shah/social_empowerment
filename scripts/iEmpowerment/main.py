8e0352285eb74d4c16d6cf7dea9189950a6b11bf
Tristan Shah <trisshah@repacss.ttu.edu>
2025-09-15 17:02:04 -0500

    starting iemp

--- scripts/iEmpowerment/main.py
+++ scripts/iEmpowerment/main.py
@@ -1,4 +1,13 @@
 from soc_emp import Dynamics
 
 if __name__ == '__main__':
-    print('hello')
+    ## simulation horizon
+    horizon = 50
+    max_power = 1.0
+    steps = 1500
+
+    ## load in xml
+    xml_path = 'xml/custom/pendulum.xml'
+    dyn = Dynamics(path = xml_path, dt = 0.01)
+
+    print(dyn)
